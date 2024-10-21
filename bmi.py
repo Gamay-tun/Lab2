2969b40d928bfcc62c150828ee266c658daae572
Gamay <GAMALIELHTTUN07.23@ichat.sp.edu.sg>
2024-10-21 15:37:20 +0800

    bmi update

--- bmi.py
+++ bmi.py
@@ -1,15 +1,36 @@
-def calculate_bmi(height,weight):
-    print("Height= "+str(height))
-    print("Weight= "+str(weight))
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    get_user_input()
 
-    bmi= weight/(height*height)
-    print("BMI is "+ str(bmi) )
+# def calculate_bmi(height,weight):
+#     print("Height= "+str(height))
+#     print("Weight= "+str(weight))
 
-    if(bmi< 18.5):
-        print("Under Weight")
-    elif(18.5<=bmi<= 25.0):
-        print("Normal Weight")
-    elif(bmi>25.0):
-        print("Over Weight")
+#     bmi= weight/(height*height)
+#     print("BMI is "+ str(bmi) )
 
-calculate_bmi(weight=57, height=1.73)
+#     if(bmi< 18.5):
+#         print("Under Weight")
+#     elif(18.5<=bmi<= 25.0):
+#         print("Normal Weight")
+#     elif(bmi>25.0):
+#         print("Over Weight")
+
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5,67, 32)")
+
+def calc_average():
+    print("calc_average")
+
+def get_user_input():
+    x=input()
+    y=x.split(",")
+
+    z= [float(item) for item in y]
+    print("your input numbers: "+str(z))
+
+
+# calculate_bmi(weight=57, height=1.73)
+
+main()
