37f2e49bdec104be71f03c75fb3883d0804867bd
Gamay <GAMALIELHTTUN07.23@ichat.sp.edu.sg>
2024-10-21 16:24:28 +0800

    median temperature added

--- Lab2.py
+++ Lab2.py
@@ -8,20 +8,29 @@ def main():
     z= [float(item) for item in y]
     calc_average_temperature(z)
     calc_min_max_temperature(z)
+    median_temperature(z)
 
 
 def display_main_menu():
     print("Temperature calculation, input all the temperatures gathered by separating with a comma ","")
 
 
+
+def median_temperature(z):
+    sorted=z.sort()
+    length= len(z)
+
+    if length%2==1:
+        median_value= z[length//2]
+    else: 
+        median_value=(z[length // 2 - 1] + z[length // 2]) / 2
+
+    print("The median temperature: "+ str(median_value))
+
 def calc_average_temperature(z): 
-     
-    
      total_sum=sum(z)
      count= len(z)
-
      average= total_sum/count
-
      print("The calculated average value of all temperature: "+ str(average))
 
 
