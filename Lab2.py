a4a93a765a3f6b851e3cfc64219c9fdca143363b
Gamay <GAMALIELHTTUN07.23@ichat.sp.edu.sg>
2024-10-21 16:10:01 +0800

    Lab2.py min max average

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,35 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    temperature= input() 
+    y=temperature.split(",")
+    z= [float(item) for item in y]
+    calc_average_temperature(z)
+    calc_min_max_temperature(z)
+
+
+def display_main_menu():
+    print("Temperature calculation, input all the temperatures gathered by separating with a comma ","")
+
+
+def calc_average_temperature(z): 
+     
+    
+     total_sum=sum(z)
+     count= len(z)
+
+     average= total_sum/count
+
+     print("The calculated average value of all temperature: "+ str(average))
+
+
+def calc_min_max_temperature(z): 
+    max_value= max(z)
+    min_value= min(z)
+    print("Maximum Value "+ str(max_value))
+    print("Minimum Value "+ str(min_value))
+
+if __name__ == "__main__":
+    main()
